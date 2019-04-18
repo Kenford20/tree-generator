0dbd9337b402b47f24a9b4174eb1836094f5e8ed
Kenford <kzhou0066@gmail.com>
2019-04-18 13:06:32 -0400

    refactored to handle random order of data

--- client/index.js
+++ client/index.js
@@ -1,19 +1,20 @@
 $(document).ready(() => {
     const treeData = [
-      {"CODEID":"8456","CODEParentID":null,"CODEVal":"8456","CODETxt":"(Root)","CODEIcon":"folder"},
-      {"CODEID":"8457","CODEParentID":"8456","CODEVal":"8457","CODETxt":"2016","CODEIcon":"file"},
-      {"CODEID":"20627","CODEParentID":"8456","CODEVal":"20627","CODETxt":"2017","CODEIcon":"folder"},
-      {"CODEID":"21124","CODEParentID":"20627","CODEVal":"21124","CODETxt":"03 March","CODEIcon":"file"},
-      {"CODEID":"21168","CODEParentID":"20627","CODEVal":"21168","CODETxt":"04 April","CODEIcon":"file"},
-      {"CODEID":"21527","CODEParentID":"20627","CODEVal":"21527","CODETxt":"06 June","CODEIcon":"folder"},
-      {"CODEID":"22304","CODEParentID":"20627","CODEVal":"22304","CODETxt":"10 October","CODEIcon":"folder"},
-      {"CODEID":"23567","CODEParentID":"22304","CODEVal":"23567","CODETxt":"Friday","CODEIcon":"file"},
-      {"CODEID":"24678","CODEParentID":"21527","CODEVal":"24678","CODETxt":"Thursday","CODEIcon":"file"}
+        {"CODEID":"21168","CODEParentID":"20627","CODEVal":"21168","CODETxt":"04 April","CODEIcon":"file"},
+        {"CODEID":"21124","CODEParentID":"20627","CODEVal":"21124","CODETxt":"03 March","CODEIcon":"file"},
+        {"CODEID":"24678","CODEParentID":"21527","CODEVal":"24678","CODETxt":"Thursday","CODEIcon":"file"},
+        {"CODEID":"8456","CODEParentID":null,"CODEVal":"8456","CODETxt":"(Root)","CODEIcon":"folder"}, 
+        {"CODEID":"21527","CODEParentID":"20627","CODEVal":"21527","CODETxt":"06 June","CODEIcon":"folder"},
+        {"CODEID":"8457","CODEParentID":"8456","CODEVal":"8457","CODETxt":"2016","CODEIcon":"file"},
+        {"CODEID":"20627","CODEParentID":"8456","CODEVal":"20627","CODETxt":"2017","CODEIcon":"folder"},
+        {"CODEID":"22304","CODEParentID":"20627","CODEVal":"22304","CODETxt":"10 October","CODEIcon":"folder"},
+        {"CODEID":"23567","CODEParentID":"22304","CODEVal":"23567","CODETxt":"Friday","CODEIcon":"file"},
     ];
     
     const hasChildren = (treeData, currElement) => {
-      let children = treeData.filter(treeElement => treeElement.CODEParentID === currElement.CODEID);
-      return children.length > 0 ? true : false;
+    //   let children = treeData.filter(treeElement => treeElement.CODEParentID === currElement.CODEID);
+    //   return children.length > 0 ? true : false;
+      return treeData.findIndex(treeElement => treeElement.CODEParentID === currElement.CODEID) >= 0;
     }
     
     const toggleFolderView = (folderNode) => {
@@ -31,9 +32,38 @@ $(document).ready(() => {
         ? $(`<img src="images/icon_${iconKey}.png"/>`) 
         : $(`<i class="fa fa-file"></i>`);
 
-        $(textNode).prepend(icon);
+      $(textNode).prepend(icon);
     }
     
+    const appendChildren = (treeData, parents) => {
+        let parentNodes = [];
+
+        parents.map(parent => {
+            let children = treeData.filter(treeElement => treeElement.CODEParentID === parent.CODEID);
+
+            children.map(child => {
+                let textNode = $(`<span>${child.CODETxt}</span>`);
+                let currNode = $(`<div id=${child.CODEID}></div>`).append(textNode);
+
+                if(hasChildren(treeData, child)) {
+                    currNode.addClass('folder-node expanded');
+                    textNode.on('click', () => toggleFolderView(currNode));
+                    parentNodes.push(child)
+                } else {
+                    currNode.addClass('file-node');
+                    textNode.on('click', () => alert(child.CODEVal));
+                }
+
+                appendIcon(textNode, child.CODEIcon);
+                $(`#${child.CODEParentID}`).append(currNode);
+            });
+        });
+
+        if(parentNodes.length > 0) {
+            return appendChildren(treeData, parentNodes);
+        }
+    }
+
     const createTree = (treeData) => {  
       let root = treeData.filter(treeElement => !treeElement.CODEParentID)[0];
       let rootNode = $(`<div id="${root.CODEID}" class="tree-root folder-node expanded"></div>`);
@@ -45,24 +75,28 @@ $(document).ready(() => {
       `).on('click', () => toggleFolderView(rootNode));
       
       $(rootNode).append(rootText);
-      $('#tree-container').append(rootNode);
-      
-      treeData.map((treeElement, i) => {
-        if(i !== 0) {
-          let textNode = $(`<span>${treeElement.CODETxt}</span>`);
-          let currNode = $(`<div id=${treeElement.CODEID}></div>`).append(textNode);
+      $('body').append(rootNode);
 
-          if(hasChildren(treeData, treeElement)) {
-            currNode.addClass('folder-node expanded');
-            textNode.on('click', () => toggleFolderView(currNode));
-          } else {
-            currNode.addClass('file-node');
-            textNode.on('click', () => alert(treeElement.CODEVal));
-          }
-          appendIcon(textNode, treeElement.CODEIcon);
-          $(`#${treeElement.CODEParentID}`).append(currNode);
-        }
-      });
+      appendChildren(treeData, [root]);
+
+
+      
+    //   treeData.map((treeElement, i) => {
+    //     if(i !== 0) {
+    //       let textNode = $(`<span>${treeElement.CODETxt}</span>`);
+    //       let currNode = $(`<div id=${treeElement.CODEID}></div>`).append(textNode);
+
+    //       if(hasChildren(treeData, treeElement)) {
+    //         currNode.addClass('folder-node expanded');
+    //         textNode.on('click', () => toggleFolderView(currNode));
+    //       } else {
+    //         currNode.addClass('file-node');
+    //         textNode.on('click', () => alert(treeElement.CODEVal));
+    //       }
+    //       appendIcon(textNode, treeElement.CODEIcon);
+    //       $(`#${treeElement.CODEParentID}`).append(currNode);
+    //     }
+    //   });
     };
   
     $('#btn').on('click', () => {
